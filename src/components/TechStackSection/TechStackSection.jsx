add0033df0e6679db6f810571091825c42166df8
Dmaf <dmaf.andinofiallos@gmail.com>
2026-03-21 08:28:08 +0100

    correciones de formato en los badges del stack

--- src/components/TechStackSection/TechStackSection.jsx
+++ src/components/TechStackSection/TechStackSection.jsx
@@ -6,6 +6,7 @@ function TechStackSection() {
         <div className={styles.card}>
             <h2>Tech Stack</h2>
             <div className={styles.stack}>
+                <TechBadge name="TypeScript" icon="fa-brands fa-typescript" color="blue" />
                 <TechBadge name="JavaScript" icon="fa-brands fa-js" color="yellow" /> 
                 <TechBadge name="CSS" icon="fa-brands fa-css3-alt" color="blue" /> 
                 <TechBadge name="HTML" icon="fa-brands fa-html5" color="orange" />
@@ -13,6 +14,9 @@ function TechStackSection() {
                 <TechBadge name="Node.js" icon="fa-brands fa-node-js" color="green" />
                 <TechBadge name="Python" icon="fa-brands fa-python" color="yellow" />
                 <TechBadge name="Docker" icon="fa-brands fa-docker" color="blue" />
+                <TechBadge name="PostgreSQL" icon="fa-brands fa-postgresql" color="blue" />
+                <TechBadge name="Figma" icon="fa-brands fa-figma" color="red" />
+                <TechBadge name="TailwindCSS" icon="fa-brands fa-tailwind-css" color="orange" /> 
             </div>
         </div>
     );
