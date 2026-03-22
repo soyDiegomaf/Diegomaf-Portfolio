2b4ed6f4a3e162ea9ca848769caf85a8a973ad8e
Dmaf <dmaf.andinofiallos@gmail.com>
2026-03-22 23:58:51 +0100

    arreglar formato en el componente de about

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -3,26 +3,26 @@ import style from './AboutSection.module.css'
 function AboutSection() {
   return (
     <section id="home" className={style.card}>
-        <h1>Diego M. Andino F.</h1>
-        <h2>Software Engineer</h2>
-        <p>Berlin, Germany</p>
-        <div className={style.links}>
-          <div className={style.cv}>
-            <a href="">
-              <i class="fa-solid fa-file-arrow-down"></i>
-              <p>Resume</p>
-            </a>
-          </div>
+      <h1>Diego M. Andino F.</h1>
+      <h2>Software Engineer</h2>
+      <p>Berlin, Germany</p>
+      <div className={style.links}>
+        <div className={style.cv}>
           <a href="">
-            <i class="fa-brands fa-linkedin-in"></i>        
-          </a>
-          <a href="">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="">
-            <i class="fa-solid fa-envelope"></i>
+            <i class="fa-solid fa-file-arrow-down"></i>
+            <p>Resume</p>
           </a>
         </div>
+        <a href="">
+          <i class="fa-brands fa-linkedin-in"></i>        
+        </a>
+        <a href="">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+      </div>
     </section>
   )
 }
