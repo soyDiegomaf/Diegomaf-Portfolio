99b918a7c05fcadc256a4c5dc55c4035b5e281ba
Dmaf <dmaf.andinofiallos@gmail.com>
2026-04-17 18:20:46 +0200

    arreglo de indentacion en el nav + conceptualizacion de los projectos

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectCard.module.css';
+
+function ProjectCard({ title, description, image, projectUrl }) {
+  return (
+    <div className="project-card">
+
+    </div>
+  );
+}
+
+export default ProjectCard;
---
+++ src/components/ProjectStack/ProjectStack.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectStack.module.css';
+
+function ProjectStack({ stack }) {
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export default ProjectStack;
