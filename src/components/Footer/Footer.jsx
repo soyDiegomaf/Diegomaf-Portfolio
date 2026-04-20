e9d630972ae0ed327f63a40b19f80c3700b63161
Dmaf <dmaf.andinofiallos@gmail.com>
2026-04-20 23:16:08 +0200

    inicio de la implementacion del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
+import style from "./Footer.module.css";
 
 function Footer() {
     return (
         <footer>
+
             <p>{new Date().getFullYear()} DiegoMAF's Portfolio.</p>
+
+            <div className={style.links}>
+                    <div className={style.cv}>
+                      <a href="">
+                        <i class="fa-solid fa-file-arrow-down"></i>
+                        <p>Resume</p>
+                      </a>
+                    </div>
+                    <a href="">
+                      <i class="fa-brands fa-linkedin-in"></i>        
+                    </a>
+                    <a href="">
+                      <i class="fa-brands fa-github"></i>
+                    </a>
+                    <a href="">
+                      <i class="fa-solid fa-envelope"></i>
+                    </a>
+                  </div>
         </footer>
     );
 }
