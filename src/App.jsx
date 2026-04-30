0746cd36a0ede9d050e462d840d9c0da86e601d6
Dmaf <dmaf.andinofiallos@gmail.com>
2026-04-30 02:00:08 +0200

    inicio de la seccion de tech stack, correcion de formato en la seccion de about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
 import NavBar from './components/NavBar/NavBar'
-import About from './sections/About/About'
+import About from './components/About/About'
+import TechBadges from './components/TechBadges/TechBadges'
 
 function App() {
   return (
     <>
     <NavBar/>
     <About/>
+    <TechBadges/>
     </>
   )
 }
---
+++ src/components/TechBadges/TechBadges.jsx
@@ -0,0 +1,40 @@
+import styles from './TechBadges.module.css'
+
+function TechBadges() {
+    const languages = ['JavaScript', 'TypeScript', 'HTML', 'CSS', 'Java', 'Python', 'SQL', 'PostgreSQL'];
+    const frameworks = ['React', 'Node.js', 'TailwindCSS', 'TensorFlow', 'Spring Boot', 'Electron', 'OpenCV', 'Express'];
+    const tools =['Git', 'Figma', 'AWS', 'Docker', 'Vercel', 'Render'];
+    const platforms = ['Windows', 'Linux', 'Mac'];
+
+    return (
+       <div className={styles.techbadges}>
+        <div className={styles.card}>
+            {languages.map((language, index) => (
+                <div key={index} className={styles.techcontainer}>
+                    {language}
+               </div>
+            ))}
+
+            {frameworks.map((framework, index) => (
+                <div key={index} className={styles.techcontainer}>
+                    {framework}
+                </div>
+            ))}
+
+            {tools.map((tool, index) => (
+                <div key={index} className={styles.techcontainer}>
+                    {tool}
+                </div>
+            ))}
+
+            {platforms.map((platform, index) => (
+                <div key={index} className={styles.techcontainer}>
+                    {platform}
+                </div>
+            ))}
+        </div>
+       </div>
+    );
+}
+
+export default TechBadges;
