abfa9d6c5110b907f9bd80ad1a782ea51092339e
Dmaf <dmaf.andinofiallos@gmail.com>
2026-05-01 02:28:13 +0200

    formato y template para experiencia y educacion terminado + navbar ahora se queda pegado arriba

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,8 +2,10 @@ import styles from './Experience.module.css'
 
 function Experience (){
     return (
-        <div className={styles.experience}>
-            <div className={styles.card}>
+        <div id="experience" className={styles.experience}>
+          <div className={styles.card}>
+            <h1>Experience & Education</h1>
+            <div className={styles.experiencecontainer}>
                 <div className={styles.type}>
                     <h2>Lorem ipsum</h2>
                 </div>
@@ -13,6 +15,27 @@ function Experience (){
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
                 </div>            
             </div>
+            <div className={styles.experiencecontainer}>
+                <div className={styles.type}>
+                    <h2>Lorem ipsum</h2>
+                </div>
+                <div className={styles.info}>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <h3>DATE - DATE</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                </div>            
+            </div>
+            <div className={styles.experiencecontainer}>
+                <div className={styles.type}>
+                    <h2>Lorem ipsum</h2>
+                </div>
+                <div className={styles.info}>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <h3>DATE - DATE</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                </div>            
+            </div>
+          </div>
         </div>
     );
 }
