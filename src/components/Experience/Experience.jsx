b82a9f8c3eabf80af1eb245692b5e1b0d18eb6a6
Dmaf <dmaf.andinofiallos@gmail.com>
2026-05-03 23:51:18 +0200

    estructura de experiencias mejoradas (divide experiencia y educacion

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -4,7 +4,7 @@ function Experience (){
     return (
         <div id="experience" className={styles.experience}>
           <div className={styles.card}>
-            <h1>Experience & Education</h1>
+            <h1>Experience</h1>
             <div className={styles.experiencecontainer}>
                 <div className={styles.type}>
                     <h2>Lorem ipsum</h2>
@@ -25,6 +25,19 @@ function Experience (){
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
                 </div>            
             </div>
+            <div className={styles.experiencecontainer}>
+                <div className={styles.type}>
+                    <h2>Lorem ipsum</h2>
+                </div>
+                <div className={styles.info}>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <h3>DATE - DATE</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                </div>            
+            </div>
+          </div>
+          <div className={styles.card}>
+            <h1>Education</h1>
             <div className={styles.experiencecontainer}>
                 <div className={styles.type}>
                     <h2>Lorem ipsum</h2>
