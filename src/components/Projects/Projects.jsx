0372c2c0a69f770d8d20ea9770b55258b0da2ea6
Dmaf <dmaf.andinofiallos@gmail.com>
2026-05-04 23:42:10 +0200

    seccion de projectos definido solo el formato esta definido

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,38 @@ function Projects () {
         <div id="projects" className={styles.projects}>
             <div className={styles.card}>
                 <h1>Projects</h1>
+                
+                <div className={styles.projectsection}>
+                <div className={styles.projectcontainer}>
+                    <div className={styles.imagesection}>
+                        <img src="" alt="project1" />
+                    </div>
+                    <div className={styles.information}>
+                        <h1>Project 1</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                    </div>            
+                </div>
+
+                <div className={styles.projectcontainer}>
+                    <div className={styles.imagesection}>
+                        <img src="" alt="project1" />
+                    </div>
+                    <div className={styles.information}>
+                        <h1>Project 1</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                    </div>            
+                </div>
+
+                <div className={styles.projectcontainer}>
+                    <div className={styles.imagesection}>
+                        <img src="" alt="project1" />
+                    </div>
+                    <div className={styles.information}>
+                        <h1>Project 1</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia, modi rerum veniam quos dignissimos cumque, quas officia maiores in nesciunt. Repudiandae rem ex repellat aliquam incidunt fugit eveniet numquam.</p>
+                    </div>            
+                </div>
+            </div>
             </div>
         </div>
     );
