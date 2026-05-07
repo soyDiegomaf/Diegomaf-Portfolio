b4c51e45c6e05a6f71a2b8ac9fe437245900fda8
Dmaf <dmaf.andinofiallos@gmail.com>
2026-05-07 23:21:38 +0200

    correcion sobre informacion en la seccion de experiencia

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -12,7 +12,7 @@ function Experience (){
                 <div className={styles.info}>
                     <h2>Pupusas & Grill</h2>
                     <h3>OCTOBER2025 - FEBRUARY 2025</h3>
-                    <h4>Honduras, Tegucigalpa</h4>
+                    <h4>Remote: Honduras, Tegucigalpa</h4>
                     <p>Designed and Developed a fully operational pos system, with an offline-first approach, designed to help facilitate sales, and manage performance and resources throughout all locations.</p>
                 </div>            
             </div>
@@ -23,7 +23,7 @@ function Experience (){
                 <div className={styles.info}>
                     <h2>Pupusas & Grill</h2>
                     <h3>APRIL 2023 - APRIL 2025</h3>
-                    <h4>Honduras, Tegucigalpa</h4>
+                    <h4>Remote: Honduras, Tegucigalpa</h4>
                     <p>Designed and Developed a responsive website for a local restaurant chain, designed to display restaurant locations, and respective menus for each location.</p>
                 </div>            
             </div>
@@ -34,7 +34,7 @@ function Experience (){
                 <div className={styles.info}>
                     <h2>UNITEC</h2>
                     <h3>MARCH 2023 - FEBRUARY 2024</h3>
-                    <h4>Honduras, Tegucigalpa</h4>
+                    <h4>Hybrid: Honduras, Tegucigalpa</h4>
                     <p>At UNITEC I went through 3 programming Labs. Once I completed these labs, I was able to assist the instructors in creating some of the problems that the following students needed to solve. Mainly in Java and C++.</p>
                 </div>            
             </div>
